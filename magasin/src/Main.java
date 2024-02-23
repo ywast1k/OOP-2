b132dfbfd4e83681e994adaa1bdbd08ce41b9074
Ywast1k <kbynevskij@yandex.ru>
2024-02-23 21:56:50 +0100

    Finish OOp

--- magasin/src/Main.java
+++ magasin/src/Main.java
@@ -2,6 +2,7 @@ import classes.Actor;
 import classes.Market;
 import classes.OrdinaryClient;
 import classes.SpecialClient;
+import classes.TaxInspector;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -9,10 +10,12 @@ public class Main {
         Actor client1 = new OrdinaryClient("Anatoliy");
         Actor client2 = new OrdinaryClient("TOlya");
         Actor client3 = new SpecialClient("President", 13);
-
+        TaxInspector client4 = new TaxInspector();
+        
         magnit.acceptToMarket(client1);
         magnit.acceptToMarket(client2);
         magnit.acceptToMarket(client3);
+        magnit.acceptToMarket(client4);
         
         magnit.update();
     }
