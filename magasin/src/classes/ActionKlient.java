0bc572a2b2ac71e06439764824d9a2184cf45611
KoNstantin_Binevskii <kbynevskij@yandex.ru>
2024-02-24 15:30:12 +0100

    Просрочка

--- magasin/src/classes/ActionKlient.java
+++ magasin/src/classes/ActionKlient.java
@@ -6,29 +6,7 @@ public class ActionKlient implements iActorBehaviour{
     private String name;
     private boolean isTakeOrder;
     private boolean isMakeOrder;
-    @Override
-    public boolean isTakeOrder() {
+
         
-        throw new UnsupportedOperationException("Unimplemented method 'isTakeOrder'");
-    }
-    @Override
-    public boolean isMakeOrder() {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'isMakeOrder'");
-    }
-    @Override
-    public void setTakeOrder(boolean isCreated) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'setTakeOrder'");
-    }
-    @Override
-    public void setMakeOrder(boolean isCreated) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'setMakeOrder'");
-    }
-    @Override
-    public Actor getActor() {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'getActor'");
-    }
+  
 }
